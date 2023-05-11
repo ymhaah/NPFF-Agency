a139045b543e29b4dcfb37e50d671e575b0b499b
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2023-05-11 22:28:43 +0300

    hero v3 35% grid 80%

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,33 +30,31 @@ function Hero({ heroLoad, heroDelay }) {
             tl.current = gsap.timeline();
 
             let galleryImage = document.querySelector(".gallery_image");
-            let galleryImageProps = galleryImage.getBoundingClientRect();
-            let galleryImageHight = galleryImageProps.height + 16;
-            let galleryImageWidth = galleryImageProps.width + 16;
 
             let animateDuration = 1;
             let animateDelay = 1;
-            // tl.current
-            //     .to(".c1", {
-            //         duration: animateDuration,
-            //         delay: animateDelay,
-            //         y: galleryImageHight,
-            //     })
-            //     .to(".c2", {
-            //         duration: animateDuration,
-            //         delay: animateDelay,
-            //         y: -galleryImageHight,
-            //     });
-            // .to(".r1", {
-            //     duration: animateDuration,
-            //     delay: animateDelay,
-            //     x: galleryImageWidth,
-            // })
-            // .to(".r2", {
-            //     duration: animateDuration,
-            //     delay: animateDelay,
-            //     x: galleryImageWidth,
-            // });
+
+            tl.current
+                .to(".c1", {
+                    duration: 1,
+                    delay: animateDelay,
+                    y: "10%",
+                })
+                .to(
+                    ".c1 .gallery_image",
+                    {
+                        duration: 0.5,
+                        // delay: animateDelay,
+                        // stagger: 0.2,
+                        scale: 0.85,
+                    },
+                    "<"
+                )
+                .to(".c1 .gallery_image", {
+                    duration: 0.5,
+                    // stagger: 0.2,
+                    scale: 1,
+                });
 
             // gsap.to(".i7", {
             //     duration: animateDuration,
@@ -89,22 +87,46 @@ function Hero({ heroLoad, heroDelay }) {
         <main className="hero" ref={hero}>
             <div className="container">
                 <div className="gallery">
-                    <div className="gallery-grid">
-                        <div className="gallery_image i1 c1"></div>
-                        <div className="gallery_image i2 "></div>
-                        <div className="gallery_image i3 c2"></div>
-                        <div className="gallery_image i4 c1"></div>
-                        <div className="gallery_image i5"></div>
-                        <div className="gallery_image i6 c2"></div>
-                        <div className="gallery_image i7 c1"></div>
-                        <div className="gallery_image i8"></div>
-                        <div className="gallery_image i9 c2"></div>
+                    <div className="c1 coll">
+                        <div className="gallery_image image-s"></div>
+                        <div className="gallery_image image-xl"></div>
+                        <div className="gallery_image image-l"></div>
+                        <div className="gallery_image image-xl"></div>
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image image-s"></div>
+                        <div className="gallery_image image-l"></div>
+                        <div className="gallery_image image-s"></div>
                     </div>
-
-                    <div className="gallery_image i10 c1"></div>
-                    <div className="gallery_image i11 "></div>
-                    <div className="gallery_image i12"></div>
-                    <div className="gallery_image i13 c2"></div>
+                    <div className="c2 coll">
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image image-l"></div>
+                        <div className="gallery_image image-s"></div>
+                        <div className="gallery_image image-xl"></div>
+                        <div className="gallery_image image-s"></div>
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image image-l"></div>
+                    </div>
+                    <div className="c3 coll">
+                        <div className="gallery_image image-xl"></div>
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image image-s"></div>
+                        <div className="gallery_image image-xl"></div>
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image image-l"></div>
+                        <div className="gallery_image image-xl"></div>
+                        <div className="gallery_image image-s"></div>
+                    </div>
+                    <div className="c4 coll">
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image"></div>
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image image-l"></div>
+                        <div className="gallery_image"></div>
+                        <div className="gallery_image image-m"></div>
+                        <div className="gallery_image image-l"></div>
+                        <div className="gallery_image image-m"></div>
+                    </div>
 
                     {/* {images.map((image) => {
                         return (
